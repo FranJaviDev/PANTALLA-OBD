8e080044981d1a5e1012f27d4c7cb3a9f318ab08
Fran <fruizvidor@educacion.navarra.es>
2024-10-01 23:06:54 +0200

    Mejoras 1.0
    Se eliminan los indicadores verticales de termopar.
    
    Se sustituye refresco por borrado entero de pantalla por borrado de texto y barra.
    
    Se acota el nivel límite a la hora de dibujar la barra para evitar desbordamientos.
    
    Se ajustan dimensiones y se añaden dos indicadores más.

--- Informacion_Motor_ELM327.ino
+++ Informacion_Motor_ELM327.ino
@@ -18,7 +18,7 @@ typedef struct{
 dato motor_1[11]={
 //  name              real  min_alam min_alert  max_alert max_alarm  bar_max    bar_min
    {"RPM",            5600.0,    0.0,      1.0,    3000.0, 4000.0,    7000.0,    0.0},
-   {"MAP",               0.0,    0.0,      1.0,    1400.0, 1850.0,    2000.0,  500.0},
+   {"MAP",               0.0,    0.0,      1.0,    1400.0, 1850.0,    2000.0,    0.0},
    {"MAF",               1.0,    0.5,      1.0,      40.0,   56.0,      60.0,    0.0},
    {"P.COMBUSTIBLE",     0.3,    0.0,      0.1,       0.6,    0.7,       1.0,    0.0},
    {"T. AGUA",           0.0,    0.0,     10.0,     110.0,  118.0,     160.0,    0.0},
@@ -48,16 +48,16 @@ int variable_a_leer;
 void dibujarIndicadoresNumericos(dato datos[]) {
   int numIndicadores = 0;
   int columnas = 2;
-  int filas = 3;
+  int filas = 4;
   int margen = 1;
 
   int anchoPantalla = lcd.width();
-  int altoPantalla = lcd.height() * 2 / 3;
+  int altoPantalla = lcd.height(); // * 2 / 3;
 
   int anchoIndicador = (anchoPantalla - (margen * (columnas + 1))) / columnas;
   int altoIndicador = (altoPantalla - (margen * (filas + 1))) / filas;
 
-  lcd.fillScreen(TFT_BLACK);             // Rellenar fondo de pantalla en negro
+  //  lcd.fillScreen(TFT_BLACK);             // Rellenar fondo de pantalla en negro
   lcd.setTextColor(TFT_WHITE);
   lcd.setTextSize(1);
 
@@ -76,6 +76,11 @@ void dibujarIndicadoresNumericos(dato datos[]) {
       lcd.setTextSize(2);
       lcd.drawString(datos[numIndicadores].nombre_indicador, x + 5, y + 5);
 
+      // Borrar el valor numérico anterior con un rectángulo negro
+      int valorX = x + 5;
+      int valorY = y + 30;
+      lcd.fillRect(valorX, valorY, anchoIndicador - 10, 30, TFT_BLACK); // Rectángulo para borrar el valor anterior
+
       // Dibujar el valor numérico del indicador
       lcd.setTextSize(3);
       lcd.drawFloat(datos[numIndicadores].valor_real, 2, x + 5, y + 30);
@@ -84,6 +89,13 @@ void dibujarIndicadoresNumericos(dato datos[]) {
       int barX = x + 10;
       int barY = y + altoIndicador - 30;
       lcd.fillRect(barX, barY, anchoIndicador - 20, 20, TFT_BLACK);
+
+      // Limitar valor_real a estar entre bar_min y bar_max
+      if (datos[numIndicadores].valor_real > datos[numIndicadores].bar_max) {
+        datos[numIndicadores].valor_real = datos[numIndicadores].bar_max;
+      } else if (datos[numIndicadores].valor_real < datos[numIndicadores].bar_min) {
+        datos[numIndicadores].valor_real = datos[numIndicadores].bar_min;
+      }
 
       // Dibujar la barra indicadora horizontal
       float range = datos[numIndicadores].bar_max - datos[numIndicadores].bar_min;
@@ -169,7 +181,7 @@ void setup() {
   lcd.fillScreen(TFT_BLACK);             // Rellenar fondo de pantalla en negro
   lcd.setTextColor(TFT_WHITE);
   lcd.setTextSize(2);
-
+  
   // Conectar al Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -198,83 +210,83 @@ void setup() {
 
 }
 
-void loop() {
+void loop(){
+
   // Código del loop  
   switch (variable_a_leer) {
+
     case 0:{
           float RPM = myELM327.rpm();
-          delay(300);
-          if (myELM327.nb_rx_state == ELM_SUCCESS)
-          {
+          if (myELM327.nb_rx_state == ELM_SUCCESS){
             motor_1[0].valor_real=RPM;
             dibujarIndicadoresNumericos(motor_1);
-            dibujarGraficosDeBarras(motor_1);
+            //dibujarGraficosDeBarras(motor_1);
             variable_a_leer=1;
-          }else if (myELM327.nb_rx_state != ELM_GETTING_MSG){
-            myELM327.printError();
-            variable_a_leer=1;
+          }else{
+            if (myELM327.nb_rx_state != ELM_GETTING_MSG){
+              myELM327.printError();
+              variable_a_leer=1;
+            };
           };
-          break;
+        break;
     };
+    
     case 1:{
           float MAP = 10.0*(float)myELM327.manifoldPressure();
-          delay(300);
-          if (myELM327.nb_rx_state == ELM_SUCCESS)
-          {      
+          if (myELM327.nb_rx_state == ELM_SUCCESS){
             motor_1[1].valor_real=MAP;
             dibujarIndicadoresNumericos(motor_1);
-            dibujarGraficosDeBarras(motor_1);
+            //dibujarGraficosDeBarras(motor_1);
             variable_a_leer=2;
-          }else if (myELM327.nb_rx_state != ELM_GETTING_MSG){
-            myELM327.printError();
-            variable_a_leer=2;
+          }else{
+            if (myELM327.nb_rx_state != ELM_GETTING_MSG){
+              myELM327.printError();
+              variable_a_leer=2;
+            };
           };
-          break;
+        break;
     };
+
     case 2:{
           float MAF = (float)myELM327.mafRate();
-          delay(300);
-          if (myELM327.nb_rx_state == ELM_SUCCESS)
-          {
+          if (myELM327.nb_rx_state == ELM_SUCCESS){
             motor_1[2].valor_real=MAF;
             dibujarIndicadoresNumericos(motor_1);
-            dibujarGraficosDeBarras(motor_1);
+            //dibujarGraficosDeBarras(motor_1);
             variable_a_leer=3;
-          }else if (myELM327.nb_rx_state != ELM_GETTING_MSG){
-            myELM327.printError();
-            variable_a_leer=3;
+          }else{
+            if (myELM327.nb_rx_state != ELM_GETTING_MSG){
+              myELM327.printError();
+              variable_a_leer=3;
+            };
           };
-          break;
+        break;
     };
     case 3:{
-          variable_a_leer=0;
-          break;
+      variable_a_leer=4;
+      break;
     };
+
     case 4:{
           float ECT = myELM327.engineCoolantTemp();
-          delay(300);
-          if (myELM327.nb_rx_state == ELM_SUCCESS)
-          {          
-          motor_1[4].valor_real=ECT;
-          dibujarIndicadoresNumericos(motor_1);
-          dibujarGraficosDeBarras(motor_1);
-          variable_a_leer=5;
-        }else if (myELM327.nb_rx_state != ELM_GETTING_MSG){
-          myELM327.printError();
-          variable_a_leer=5;
-        };
+          if (myELM327.nb_rx_state == ELM_SUCCESS){
+            motor_1[4].valor_real=ECT;
+            dibujarIndicadoresNumericos(motor_1);
+            //dibujarGraficosDeBarras(motor_1);
+            variable_a_leer=0;
+          }else{
+            if (myELM327.nb_rx_state != ELM_GETTING_MSG){
+              myELM327.printError();
+              variable_a_leer=0;
+            };
+          };
+        break;
     };
-    case 5:{
-          variable_a_leer=6;
-          break;
-    };
-    case 6:{
-          variable_a_leer=0;
-          break;
-    };
+
     default:{
           variable_a_leer=0;
           break;
     };
   };
+  
 };
